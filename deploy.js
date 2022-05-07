d57d0a093d22d9bf1dc5eeda474e2e1747f4dac7
Vignesh Panneerselvam <vigneshpanneerselvam1998@gmail.com>
2022-05-07 11:19:58 +0530

    deloy code added

--- deploy.js
+++ deploy.js
@@ -1,1 +1,17 @@
-// deploy code will go here
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider('worry smart rude ladder scissors target bean gain utility lobster throw unaware', 'https://rinkeby.infura.io/v3/2ef07da1e7a34a4fbc9af4e9032ff5b1');
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+    console.log('Attempting to deploy from', accounts[0]);
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there!'] })
+        .send({ gas: '1000000', from: accounts[0] });
+    console.log('Contract deployed to', result.options.address);
+    provider.engine.stop();
+};
+deploy();
